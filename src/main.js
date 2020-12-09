3c676c2986a92e104aef5801c29c58fc8e478466
Fer619 <fbriceno@ucol.mx>
2020-12-09 16:07:14 -0600

    implementation of factorial()}

--- src/main.js
+++ src/main.js
@@ -1,3 +1,20 @@
-export default class App {}
+export default class App {
+
+       factorial(numero){
+
+        let factorial= 1;
+        
+        for(let i=2; i<=numero; i++){
+
+            factorial= factorial * i
+        }
+
+        return factorial;
+    }
+
+    
+}
 
 let app = new App();
+
+console.log(app.factorial(6));
